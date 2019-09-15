a4d93265a688108b2caff97ea51ae4c2e89cb1bd
WangXing <wx2018@mail.ustc.edu.cn>
2019-09-15 22:47:52 +0800

    gitTest-2019-9-15-02

--- gitmodule/src/main/java/com/wx/domain/User.java
+++ gitmodule/src/main/java/com/wx/domain/User.java
@@ -4,4 +4,8 @@ public class User {
     private String name;
     private Integer age;
     private String address;
+
+    public void call(String name){
+        System.out.println("name"+name);
+    }
 }
